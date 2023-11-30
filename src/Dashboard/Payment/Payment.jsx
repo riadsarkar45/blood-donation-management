4bd963079762412af3c2525d668ba3d8331900e8
riadsarkar45 <riadsarkar45@gmail.com>
2023-11-30 19:28:26 +0600

    payment page updated

--- src/Dashboard/Payment/Payment.jsx
+++ src/Dashboard/Payment/Payment.jsx
@@ -6,6 +6,9 @@ const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_STRIPE_KEY);
 const Payment = () => {
     return (
         <div>
+            <div className="shadow-sm w-full p-5 text-2xl">
+                <h2>Add New Blog</h2>
+            </div>
             <Helmet>
                 <title>Donation</title>
             </Helmet>
